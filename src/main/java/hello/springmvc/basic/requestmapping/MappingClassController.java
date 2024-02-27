b637f892c96c1d7b9b1304ac60e50e90dfc60824
ratellll <latellbin@naver.com>
2024-02-27 23:33:05 +0900

    feat: 요청매핑연습2

--- src/main/java/hello/springmvc/basic/requestmapping/MappingClassController.java
+++ src/main/java/hello/springmvc/basic/requestmapping/MappingClassController.java
@@ -9,39 +9,36 @@ public class MappingClassController {
 
 
     /**
-     회원 목록 조회: GET  '/users'
-     회원 등록: POST    /users
-     회원 조회: GET      /users/{userId}
-     회원수정: PATCH    /users/{userId}
-     회원 삭제: DELETE   /users/{userId}
-      */
+     * 회원 목록 조회: GET  '/users'
+     * 회원 등록: POST    /users
+     * 회원 조회: GET      /users/{userId}
+     * 회원수정: PATCH    /users/{userId}
+     * 회원 삭제: DELETE   /users/{userId}
+     */
+
     @GetMapping
-    public String users() {
+    public String user() {
         return "get users";
     }
+
     @PostMapping
     public String addUser() {
         return "post user";
     }
 
-
     @GetMapping("/{userId}")
-    public String findUser(@PathVariable String  userId) {
+    public String findUser(@PathVariable String userId) {
         return "get userId=" + userId;
     }
 
-
     @PatchMapping("/{userId}")
-    public String updateUser(@PathVariable String  userId) {
-        return "update userId=" + userId;
+    public String updateUser(@PathVariable String userId) {
+        return "update userId= " + userId;
     }
 
-
-    @DeleteMapping("/{userId}")
-    public String deleteUser(@PathVariable String  userId) {
+    @DeleteMapping({"/{userId}"})
+    public String deleteUser(@PathVariable String userId) {
         return "delete userId=" + userId;
+
     }
-
-
-
 }
