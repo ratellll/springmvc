9cc626089d27a4650fd3db919b0254754af9dce4
ratellll <latellbin@naver.com>
2024-02-29 19:15:22 +0900

    feat: RequestParam 매핑연습

--- src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -7,8 +7,11 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.io.IOException;
+import java.util.Map;
 
 
 @Slf4j
@@ -16,12 +19,7 @@ import java.io.IOException;
 public class RequestParamController {
 
 
-    /**
-     * @RequestParam 사용
-     * - 파라미터 이름으로 바인딩
-     * @ResponseBody 추가
-     * - View 조회를 무시하고, HTTP message body에 직접 해당 내용 입력
-     */
+
     @RequestMapping("/request-param-v1")
     public void requestParamV1(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String username = request.getParameter("username");
@@ -33,16 +31,56 @@ public class RequestParamController {
         response.getWriter().write("ok");
     }
 
+
+
+    /**
+     * @RequestParam 사용
+     * - 파라미터 이름으로 바인딩
+     * @ResponseBody 추가
+     * - View 조회를 무시하고, HTTP message body에 직접 해당 내용 입력
+     */
+    @ResponseBody
+    @RequestMapping("/request-param-v2")
+    public String requestParamV2(@RequestParam("username") String memberName,
+                               @RequestParam("age") String memberage,
+                                @RequestParam("size") String membersize,
+                                           @RequestParam("account") String memberaccount) throws IOException {
+
+        log.info(memberName + "====" + memberage + "====" + membersize + "====" + memberaccount);
+        return "ok";
+    }
+
+
     /**
      * @RequestParam 사용
      * HTTP 파라미터 이름이 변수 이름과 같으면 @RequestParam(name="xx") 생략 가능
+     * 변수이름은 무조건 같아야함
      */
+    @ResponseBody
+    @RequestMapping("/request-param-v3")
+    public String requestParamV3(@RequestParam String username,
+                                 @RequestParam int age,
+                                 @RequestParam int size,
+                                 @RequestParam  int account) throws IOException {
 
-
+        log.info(username + "====" + age + "====" + size + "====" + account);
+        return "ok";
+    }
     /**
      * @RequestParam 사용
      * String, int 등의 단순 타입이면 @RequestParam 도 생략 가능
      */
+
+    @ResponseBody
+    @RequestMapping("/request-param-v4")
+    public String requestParamV4(String username,
+                                  int age,
+                                  int size,
+                                   int account) throws IOException {
+
+        log.info(username + "====" + age + "====" + size + "====" + account);
+        return "ok";
+    }
 
     /**
      * @RequestParam.required
@@ -58,6 +96,16 @@ public class RequestParamController {
      * required 가 true 이면 파라미터 필수
      * required=false 이면 파라미터 필수 아님
      */
+    @ResponseBody
+    @RequestMapping("/request-param-required")
+    public String requestParamRequired(@RequestParam(required = false) String username,
+                                       @RequestParam(required = false) Integer age,
+                                       @RequestParam(required = false) Integer size,
+                                       @RequestParam(required = false) Integer account) throws IOException {
+
+        log.info(username + "====" + age + "====" + size + "====" + account);
+        return "ok";
+    }
 
 
     /*
@@ -67,6 +115,23 @@ public class RequestParamController {
      * "guest" 라는 문자가 들어올 경우에는 defaultValue가 적용되지 않음
      * "-1" 라는 문자가 들어올 경우에는 defaultValue가 적용됨
      */
+    @ResponseBody
+    @RequestMapping("/request-param-default")
+    public String requestParamDefault(@RequestParam(required = false, defaultValue = "-1") String username,
+                                       @RequestParam(required = false ,defaultValue = "-1") Integer age,
+                                       @RequestParam(required = false) Integer size,
+                                       @RequestParam(required = false) Integer account) throws IOException {
+
+        log.info(username + "====" + age + "====" + size + "====" + account);
+        return "ok";
+    }
 
 
+    @ResponseBody
+    @RequestMapping("/request-param-map")
+    public String requestParamMap(@RequestParam Map<String ,Object> paramMap) throws IOException {
+
+        log.info(paramMap.get("username") + "====" + paramMap.get("age") + "====" + paramMap.get("size") + "====" +  paramMap.get("account"));
+        return "ok";
+    }
 }
