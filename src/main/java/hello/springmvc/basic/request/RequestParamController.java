2c0cdc5b888a59236e450e56121e5d1ac93473af
ratellll <latellbin@naver.com>
2024-02-29 18:46:47 +0900

    feat: Reque바인딩stParam

--- src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -1,23 +1,19 @@
 package hello.springmvc.basic.request;
 
 
-import hello.springmvc.basic.HelloData;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-
 
 import java.io.IOException;
-import java.util.Map;
 
 
 @Slf4j
 @Controller
 public class RequestParamController {
-
 
 
     /**
@@ -26,13 +22,21 @@ public class RequestParamController {
      * @ResponseBody 추가
      * - View 조회를 무시하고, HTTP message body에 직접 해당 내용 입력
      */
+    @RequestMapping("/request-param-v1")
+    public void requestParamV1(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String username = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age"));
+        int size = Integer.parseInt(request.getParameter("size"));
+        int account = Integer.parseInt(request.getParameter("account"));
+        log.info(username + "====" + age + "====" + size + "====" + account);
 
+        response.getWriter().write("ok");
+    }
 
     /**
      * @RequestParam 사용
      * HTTP 파라미터 이름이 변수 이름과 같으면 @RequestParam(name="xx") 생략 가능
      */
-
 
 
     /**
@@ -57,15 +61,12 @@ public class RequestParamController {
 
 
     /*
-    * @RequestParam - defaultValue 사용
-    * 참고: defaultValue는 빈 문자의 경우에도 적용
-    *
-    * "guest" 라는 문자가 들어올 경우에는 defaultValue가 적용되지 않음
-    * "-1" 라는 문자가 들어올 경우에는 defaultValue가 적용됨
+     * @RequestParam - defaultValue 사용
+     * 참고: defaultValue는 빈 문자의 경우에도 적용
+     *
+     * "guest" 라는 문자가 들어올 경우에는 defaultValue가 적용되지 않음
+     * "-1" 라는 문자가 들어올 경우에는 defaultValue가 적용됨
      */
-
-
-
 
 
 }
